178978a4f6915581d673b52c339cf4c43d3210c1
Ramago1715 <ramago1715@gmail.com>
2023-10-27 20:36:24 +0200

    subida general

---
+++ app/src/main/java/com/example/questions/Question.java
@@ -0,0 +1,33 @@
+package com.example.questions;
+
+import android.widget.Button;
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+
+    public Question(int textResId,boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+
+    }
+
+
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int mTextResId) {
+        this.mTextResId = mTextResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean mAnswerTrue) {
+        this.mAnswerTrue = mAnswerTrue;
+    }
+}
